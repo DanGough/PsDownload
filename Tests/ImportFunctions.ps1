38bfdea546762dc8a0dae8638376021272864798
Dan Gough <dan.gough@oriium.com>
2023-12-11 15:06:52 +0000

    Corrected typo

--- Tests/ImportFunctions.ps1
+++ Tests/ImportFunctions.ps1
@@ -1,7 +1,7 @@
 # For quickly dot-sourcing all functions into current session for testing
 
-$Public = @(Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, '..\Nevergreen\Public', '*.ps1')))
-$Private = @(Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, '..\Nevergreen\Private', '*.ps1')) -ErrorAction Ignore)
+$Public = @(Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, '..\PsDownload\Public', '*.ps1')))
+$Private = @(Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, '..\PsDownload\Private', '*.ps1')) -ErrorAction Ignore)
 
 ForEach ($Import in @($Public + $Private)) {
     Try {
